6e9375db1ed9e92c0c4006d61a96488b37c6a90b
linda <linda8832825@gmail.com>
2023-06-25 20:15:52 +0800

    Initial commit

---
+++ LBP.v
@@ -0,0 +1,240 @@
+
+`timescale 1ns/10ps
+module LBP ( clk, reset, gray_addr, gray_req, gray_ready, gray_data, lbp_addr, lbp_valid, lbp_data, finish);
+input   	    clk;
+input   	    reset;
+
+input   	    gray_ready;
+input   [7:0] 	gray_data;
+
+output reg          [13:0] 	gray_addr;
+output reg         	gray_req;
+output reg  [13:0] 	lbp_addr;
+output reg	        lbp_valid;
+output reg  [7:0] 	lbp_data;
+output reg 	        finish;
+
+reg     [1:0]   cur_state, nx_state;
+
+reg     [3:0]   count,i;
+
+reg     [7:0]   data    [0:8];
+reg     [13:0]  pt;
+
+reg     s       [0:8];
+reg     [7:0]   sol;
+
+parameter       STATE_IDLE=2'b00, STATE_RD=2'b01, STATE_CAL=2'b10, STATE_SD=2'b11;
+//====================================================================
+
+
+//================================================================
+// STATE_RD
+//================================================================
+
+//count
+always@(posedge clk or posedge reset)begin
+    if(reset) count <= 4'd0;
+    else begin
+        case(cur_state)
+            STATE_IDLE: count <= 0;
+            STATE_RD:   count <= count+1;
+            STATE_CAL:  count <= 0;
+            STATE_SD:   count <= 0;
+        endcase
+    end
+end
+
+//pt
+always@(posedge clk or posedge reset)begin
+    if(reset) pt <= 14'd0;
+    else begin
+        case(cur_state)
+            STATE_IDLE: pt <= 14'd0;
+            STATE_RD:  pt <= pt; 
+            STATE_CAL: pt <= pt;
+            STATE_SD: begin
+                if((pt + 14'd3) % 8'd128 == 0) pt <= pt + 14'd3;
+                else pt <= pt +14'd1;
+            end
+        endcase
+    end
+end
+
+//gray_addr
+always@(posedge clk or posedge reset)begin
+    if(reset) begin
+        gray_addr <= 14'd0;
+    end
+    else begin
+        if(cur_state == STATE_RD) begin
+            // if(pt % 8'd128 == 0)begin
+                case(count)
+                    0: gray_addr <= pt;
+                    1: gray_addr <= pt + 14'd1;
+                    2: gray_addr <= pt + 14'd2;
+                    3: gray_addr <= pt + 14'd128;
+                    4: gray_addr <= pt + 14'd129;
+                    5: gray_addr <= pt + 14'd130;
+                    6: gray_addr <= pt + 14'd256;
+                    7: gray_addr <= pt + 14'd257;
+                    8: gray_addr <= pt + 14'd258;
+                    default: gray_addr <= 14'd0;
+                endcase
+            // end
+        //     else begin
+        //         case(count)
+        //             0: gray_addr = pt + 14'd2;
+        //             1: gray_addr = pt + 14'd130;
+        //             2: gray_addr = pt + 14'd258;
+        //         endcase
+        //     end
+        end
+        else gray_addr <= 14'd0;
+    end
+end 
+
+//data
+always@(posedge clk or posedge reset)begin
+    if(reset) begin
+        for(i=0 ; i<=4'd8 ; i=i+1)begin
+            data[i] <= 4'd0;
+        end
+    end
+    else begin
+        // if(pt % 8'd128 == 0) begin
+            data[count - 4'd1] <= (count >= 4'd1) ? gray_data : data[count];
+        // end
+        // else begin
+        //     case(count)
+        //         0: begin
+        //             data[0] <= data[1];
+        //             data[1] <= data[2];
+        //             data[3] <= data[4];
+        //             data[4] <= data[5];
+        //             data[6] <= data[7];
+        //             data[7] <= data[8];
+                    
+        //             data[2] <=gray_data;
+        //         end
+        //         1: data[5] <=gray_data;
+        //         2: data[8] <=gray_data;
+        //     endcase
+        // end
+    end
+end
+
+//================================================================
+// STATE_CAL
+//================================================================
+
+//s
+always@(*) begin
+    if(cur_state == STATE_CAL)begin
+        for(i=0 ; i<=4'd8 ; i=i+1)begin
+            s[i] = (data[i] >= data[4]) ? 1 : 0 ;
+        end
+    end
+    else begin
+        for(i=0 ; i<=4'd8 ; i=i+1)begin
+            s[i] = 0;
+        end
+    end
+end
+
+//sol
+always@(*)begin
+    if(cur_state == STATE_CAL) begin
+        sol = s[0]*2**0 +
+              s[1]*2**1 +
+              s[2]*2**2 +
+              s[3]*2**3 +
+              s[5]*2**4 +
+              s[6]*2**5 +
+              s[7]*2**6 +
+              s[8]*2**7 ;
+    end
+    else sol = sol;
+end
+
+//================================================================
+// STATE_SD
+//================================================================
+
+//lbp_addr
+always@(posedge clk or posedge reset)begin
+    if(reset) lbp_addr <= 14'd0;
+    else begin
+        if(nx_state == STATE_SD) begin
+            lbp_addr <= pt + 14'd129;
+        end
+        else lbp_addr <= lbp_addr;
+    end
+end
+
+//lbp_data
+always@(posedge clk or posedge reset)begin
+    if(reset) lbp_data <= 8'd0;
+    else begin
+        if(nx_state == STATE_SD) begin
+            lbp_data <= sol;
+        end
+        else lbp_data <= 8'd0;
+    end
+end
+
+
+//================================================================
+// FSM
+//================================================================
+always@(posedge clk or posedge reset) begin
+    if(reset) cur_state <= STATE_IDLE;
+    else begin
+        cur_state <= nx_state; 
+    end
+end
+
+always@(*)begin
+    case(cur_state)
+        STATE_IDLE: nx_state <= (gray_ready) ? STATE_RD : STATE_IDLE;
+        STATE_RD:   nx_state <= (gray_req) ? STATE_RD : STATE_CAL;
+        STATE_CAL:  nx_state <= STATE_SD;
+        STATE_SD:   nx_state <= STATE_RD;
+    endcase
+end
+
+
+
+
+//================================================================
+// signal control
+//================================================================
+always @(*) begin
+    case(cur_state)
+        STATE_IDLE:begin
+            gray_req=0;
+            lbp_valid=0;
+            finish=0;
+        end
+        STATE_RD:begin
+            // if((pt % 8'd128) == 0) gray_req = (count > 4'd9) ? 0 : 1;
+            // else gray_req = (count > 4'd3) ? 0 : 1;
+            gray_req = (count > 4'd9) ? 0 : 1;
+            lbp_valid=0;
+            finish = (lbp_addr == 14'd16254) ? 1 : 0;
+        end
+        STATE_CAL:begin
+            gray_req=0;
+            lbp_valid=0;
+            finish=0;
+        end
+        STATE_SD:begin
+            gray_req=0;
+            lbp_valid=1;
+            finish=0;
+        end
+    endcase
+end
+
+//====================================================================
+endmodule
---
+++ nWaveLog/nWave.cmd
@@ -0,0 +1,158 @@
+wvResizeWindow -win $_nWave1 9 32 1902 991
+wvSetPosition -win $_nWave1 {("G1" 0)}
+wvOpenFile -win $_nWave1 {/home/t111368148/Desktop/2016_LBP/LBP.fsdb}
+wvGetSignalOpen -win $_nWave1
+wvGetSignalSetScope -win $_nWave1 "/testfixture"
+wvGetSignalClose -win $_nWave1
+wvRestoreSignal -win $_nWave1 "/home/t111368148/Desktop/2016_LBP/signal.rc" \
+           -overWriteAutoAlias on -appendSignals on
+wvResizeWindow -win $_nWave1 1920 23 1920 1009
+wvSelectSignal -win $_nWave1 {( "G1" 9 )} 
+wvSelectSignal -win $_nWave1 {( "G1" 34 )} 
+wvZoomAll -win $_nWave1
+wvSetCursor -win $_nWave1 2479058356.288391 -snap {("G1" 16)}
+wvSetCursor -win $_nWave1 2238940138.532709 -snap {("G1" 34)}
+wvDisplayGridCount -win $_nWave1 -off
+wvGetSignalClose -win $_nWave1
+wvReloadFile -win $_nWave1
+wvZoomOut -win $_nWave1
+wvZoomOut -win $_nWave1
+wvZoomOut -win $_nWave1
+wvZoomOut -win $_nWave1
+wvZoomIn -win $_nWave1
+wvZoomIn -win $_nWave1
+wvZoomIn -win $_nWave1
+wvZoomIn -win $_nWave1
+wvZoomIn -win $_nWave1
+wvZoomIn -win $_nWave1
+wvZoomIn -win $_nWave1
+wvZoomIn -win $_nWave1
+wvZoomIn -win $_nWave1
+wvZoomIn -win $_nWave1
+wvZoomIn -win $_nWave1
+wvZoomIn -win $_nWave1
+wvZoomIn -win $_nWave1
+wvZoomIn -win $_nWave1
+wvZoomIn -win $_nWave1
+wvZoomIn -win $_nWave1
+wvZoomIn -win $_nWave1
+wvZoomIn -win $_nWave1
+wvSetCursor -win $_nWave1 2005553153.760856
+wvZoomOut -win $_nWave1
+wvZoomOut -win $_nWave1
+wvSetCursor -win $_nWave1 2067609338.541936 -snap {("G1" 34)}
+wvSetCursor -win $_nWave1 2067605122.639632 -snap {("G1" 34)}
+wvResizeWindow -win $_nWave1 0 23 1920 1009
+wvSetCursor -win $_nWave1 2314881165.801791 -snap {("G1" 34)}
+wvSetCursor -win $_nWave1 2314881361.920033 -snap {("G1" 34)}
+wvSetCursor -win $_nWave1 2401672638.314284 -snap {("G1" 34)}
+wvSetCursor -win $_nWave1 2367433434.314008 -snap {("G1" 34)}
+wvSetCursor -win $_nWave1 2314637755.592335 -snap {("G1" 34)}
+wvZoomIn -win $_nWave1
+wvZoomIn -win $_nWave1
+wvSearchNext -win $_nWave1
+wvSearchNext -win $_nWave1
+wvSearchNext -win $_nWave1
+wvSearchNext -win $_nWave1
+wvSearchNext -win $_nWave1
+wvSearchNext -win $_nWave1
+wvSearchNext -win $_nWave1
+wvSetCursor -win $_nWave1 2331002520.280580
+wvSetCursor -win $_nWave1 2331004947.243828
+wvGoToTime -win $_nWave1 2331014947.244
+wvGoToTime -win $_nWave1 2331024947.244
+wvGoToTime -win $_nWave1 2331044947.244
+wvGoToTime -win $_nWave1 2331084947.244
+wvGoToTime -win $_nWave1 2331184947.244
+wvGoToTime -win $_nWave1 2311184947.244
+wvGoToTime -win $_nWave1 2511184947.244
+wvGoToTime -win $_nWave1 2411184947.244
+wvGoToTime -win $_nWave1 2411184947
+wvGoToTime -win $_nWave1 2411100000
+wvGoToTime -win $_nWave1 2411000000
+wvGoToTime -win $_nWave1 2410000000
+wvGoToTime -win $_nWave1 2400000000
+wvSetCursor -win $_nWave1 1593.460718 -snap {("G1" 29)}
+wvSetCursor -win $_nWave1 2353.418907 -snap {("G1" 29)}
+wvSetCursor -win $_nWave1 4461.690011 -snap {("G1" 30)}
+wvSetCursor -win $_nWave1 3603.672701
+wvSetCursor -win $_nWave1 4951.985617
+wvSetCursor -win $_nWave1 5050.044738
+wvSetCursor -win $_nWave1 5883.547268
+wvSetCursor -win $_nWave1 8236.966175
+wvGoToTime -win $_nWave1 10000000
+wvGoToTime -win $_nWave1 100000000
+wvGoToTime -win $_nWave1 200000000
+wvGoToTime -win $_nWave1 200500000
+wvGoToTime -win $_nWave1 201000000
+wvGoToTime -win $_nWave1 202000000
+wvGoToTime -win $_nWave1 203000000
+wvGoToTime -win $_nWave1 205000000
+wvGoToTime -win $_nWave1 208000000
+wvGoToTime -win $_nWave1 208000000
+wvGoToTime -win $_nWave1 210000000
+wvGoToTime -win $_nWave1 215000000
+wvGoToTime -win $_nWave1 219000000
+wvGoToTime -win $_nWave1 220000000
+wvGoToTime -win $_nWave1 230000000
+wvGoToTime -win $_nWave1 210000000
+wvGoToTime -win $_nWave1 2400000000
+wvGoToTime -win $_nWave1 2350000000
+wvGoToTime -win $_nWave1 2355000000
+wvSetCursor -win $_nWave1 2354628586.369338 -snap {("G1" 36)}
+wvSearchNext -win $_nWave1
+wvGoToTime -win $_nWave1 13500000
+wvGoToTime -win $_nWave1 135000000
+wvSetCursor -win $_nWave1 134984739.549270 -snap {("G1" 20)}
+wvDisplayGridCount -win $_nWave1 -off
+wvGetSignalClose -win $_nWave1
+wvReloadFile -win $_nWave1
+wvSetCursor -win $_nWave1 257988660.418609 -snap {("G1" 32)}
+wvSetCursor -win $_nWave1 257315083.458788
+wvSetCursor -win $_nWave1 257987845.025517
+wvSetCursor -win $_nWave1 257988482.409804
+wvSetCursor -win $_nWave1 257989315.912334 -snap {("G1" 35)}
+wvSetCursor -win $_nWave1 257989732.663599 -snap {("G2" 0)}
+wvDisplayGridCount -win $_nWave1 -off
+wvGetSignalClose -win $_nWave1
+wvReloadFile -win $_nWave1
+wvSetCursor -win $_nWave1 29319.677216 -snap {("G1" 35)}
+wvDisplayGridCount -win $_nWave1 -off
+wvGetSignalClose -win $_nWave1
+wvReloadFile -win $_nWave1
+wvSetCursor -win $_nWave1 20665.959777 -snap {("G1" 35)}
+wvSetCursor -win $_nWave1 19317.646861 -snap {("G1" 34)}
+wvSetCursor -win $_nWave1 24196.088137 -snap {("G2" 0)}
+wvDisplayGridCount -win $_nWave1 -off
+wvGetSignalClose -win $_nWave1
+wvReloadFile -win $_nWave1
+wvSetCursor -win $_nWave1 20616.930216 -snap {("G1" 35)}
+wvSetCursor -win $_nWave1 19415.705982 -snap {("G1" 33)}
+wvSetCursor -win $_nWave1 21499.462306 -snap {("G2" 0)}
+wvDisplayGridCount -win $_nWave1 -off
+wvGetSignalClose -win $_nWave1
+wvReloadFile -win $_nWave1
+wvSetCursor -win $_nWave1 17135.831416 -snap {("G1" 27)}
+wvDisplayGridCount -win $_nWave1 -off
+wvGetSignalClose -win $_nWave1
+wvReloadFile -win $_nWave1
+wvSetCursor -win $_nWave1 30030.605844 -snap {("G1" 29)}
+wvDisplayGridCount -win $_nWave1 -off
+wvGetSignalClose -win $_nWave1
+wvReloadFile -win $_nWave1
+wvSetCursor -win $_nWave1 5540.340343 -snap {("G1" 10)}
+wvSetCursor -win $_nWave1 15664.944599 -snap {("G1" 10)}
+wvSetCursor -win $_nWave1 6839.623698 -snap {("G1" 10)}
+wvSetCursor -win $_nWave1 16841.654053 -snap {("G1" 10)}
+wvSetCursor -win $_nWave1 6815.108918 -snap {("G1" 10)}
+wvSetCursor -win $_nWave1 8163.421833 -snap {("G1" 22)}
+wvDisplayGridCount -win $_nWave1 -off
+wvGetSignalClose -win $_nWave1
+wvReloadFile -win $_nWave1
+wvSetCursor -win $_nWave1 8138.907053 -snap {("G1" 14)}
+wvSetCursor -win $_nWave1 6815.108918 -snap {("G1" 10)}
+wvSetCursor -win $_nWave1 20862.078019 -snap {("G1" 30)}
+wvDisplayGridCount -win $_nWave1 -off
+wvGetSignalClose -win $_nWave1
+wvReloadFile -win $_nWave1
+wvSetCursor -win $_nWave1 6815.108918 -snap {("G1" 13)}
